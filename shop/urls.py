454a6fa33cdbe8f60f10f28d49c57a3b863beae3
Vladyslav Bridun <erikvlad22052005@gmail.com>
2023-04-10 00:03:39 +0300

    Add cards with product

--- shop/urls.py
+++ shop/urls.py
@@ -19,7 +19,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about'),
+    path('product/', views.product, name='product'),
 
 ]
 
